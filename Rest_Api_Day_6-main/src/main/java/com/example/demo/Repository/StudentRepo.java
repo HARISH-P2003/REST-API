79604871adfd0b97ff96a7ae4e0d14514543fe90
HARISH-P2003 <108795314+HARISH-P2003@users.noreply.github.com>
2023-09-01 14:38:45 +0530

    Delete Rest_Api_Day_6-main directory

--- Rest_Api_Day_6-main/src/main/java/com/example/demo/Repository/StudentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.StudentEntity;
-
-public interface StudentRepo extends JpaRepository<StudentEntity, Integer> {
-
-}
