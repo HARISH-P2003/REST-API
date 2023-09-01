4fd26d3d3475a6db15c08d0feac7af21f5ac2cae
HARISH-P2003 <108795314+HARISH-P2003@users.noreply.github.com>
2023-09-01 14:45:00 +0530

    Delete PROJECT(1)/PROJECT/OnlineGroceryShopping directory

--- PROJECT(1)/PROJECT/OnlineGroceryShopping/src/main/java/com/example/demo/Repository/ItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.ItemDetails;
-
-public interface ItemRepo extends JpaRepository<ItemDetails, Integer>{
-
-}
--- PROJECT(1)/PROJECT/OnlineGroceryShopping/src/main/java/com/example/demo/Repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.UserDetails;
-
-public interface UserRepo extends JpaRepository<UserDetails, Long> {
-
-}
